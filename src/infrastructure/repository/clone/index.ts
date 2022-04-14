24257984e55d60216a89aa5cd7d7ac91432e3d3a
kazuhe <omc.ohara@gmail.com>
2022-04-15 01:27:43 +0900

    fix clone

--- src/infrastructure/repository/clone/index.ts
+++ src/infrastructure/repository/clone/index.ts
@@ -1,14 +1,9 @@
 import child_process from "child_process";
 import { Clone } from "@/domain/repository";
 
-/**
- * リポジトリを clone する
- * @param remote remote path
- * @param local local path
- */
 export const clone =
   (exec: typeof child_process.exec): Clone =>
-  async (remote, local) => {
+  (remote, local) => {
     return new Promise((resolve, reject) => {
       exec(`git clone ${remote} ${local}`, (error, _, stderr) => {
         if (error) {
