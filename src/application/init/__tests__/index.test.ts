643cb8f84705f698a235fb7ad7dcd98042d0bd32
kazuhe <omc.ohara@gmail.com>
2022-06-11 07:17:31 +0900

    Added init test

--- src/application/init/__tests__/index.test.ts
+++ src/application/init/__tests__/index.test.ts
@@ -1,6 +1,7 @@
 import { init } from "..";
+import os from "os";
 
-describe("clone", () => {
+describe("init", () => {
   const cloneMock = jest.fn();
   const questionMock = jest.fn();
 
@@ -9,10 +10,55 @@ describe("clone", () => {
     questionMock.mockClear();
   });
 
-  test("question が実行され、空文字が返却された場合は false を返すこと", async () => {
-    cloneMock.mockResolvedValue("");
-    const target = init(cloneMock, questionMock);
+  describe("question が空文字で解決された場合", () => {
+    test("clone が実行されず false を返すこと", async () => {
+      questionMock.mockResolvedValue("");
+      cloneMock.mockResolvedValue("");
+      const target = init(cloneMock, questionMock);
 
-    expect(await target()).toBe(false);
+      expect(await target()).toBe(false);
+      expect(cloneMock).not.toHaveBeenCalled();
+    });
+  });
+
+  describe("question が空文字以外で解決された場合", () => {
+    test("clone が適切な引数で実行されること", async () => {
+      questionMock.mockResolvedValue("path/foo");
+      cloneMock.mockResolvedValue("");
+      const target = init(cloneMock, questionMock);
+
+      await target();
+      expect(cloneMock).toHaveBeenCalledWith(
+        "path/foo",
+        os.homedir() + "/gitmemo"
+      );
+    });
+
+    test("clone が解決された場合は true を返すこと", async () => {
+      questionMock.mockResolvedValue("path/foo");
+      cloneMock.mockResolvedValue("");
+      const target = init(cloneMock, questionMock);
+
+      expect(await target()).toBe(true);
+    });
+
+    test("clone が拒否された場合は false を返すこと", async () => {
+      questionMock.mockResolvedValue("path/foo");
+      cloneMock.mockRejectedValue("Error Message");
+      const target = init(cloneMock, questionMock);
+
+      expect(await target()).toBe(false);
+    });
+  });
+
+  describe("question が拒否された場合", () => {
+    test("clone が実行されず false を返すこと", async () => {
+      questionMock.mockRejectedValue("");
+      cloneMock.mockResolvedValue("");
+      const target = init(cloneMock, questionMock);
+
+      expect(await target()).toBe(false);
+      expect(cloneMock).not.toHaveBeenCalled();
+    });
   });
 });
--- src/application/init/index.ts
+++ src/application/init/index.ts
@@ -10,17 +10,16 @@ const HOME_DIR = os.homedir() + "/gitmemo";
 type Init = (clone: Clone, question: Question) => () => Promise<boolean>;
 
 export const init: Init = (clone, question) => async () => {
-  const remote = await question("リポジトリの URL を入力してください: ");
+  const remote = await question("リポジトリの URL を入力してください: ").catch(() => "")
   if (!remote) return false;
 
-  await clone(remote, HOME_DIR)
+  return clone(remote, HOME_DIR)
     .then(() => {
       console.log(`🎉 "${HOME_DIR}" に GitMemo をセットアップしました`);
       return true;
     })
     .catch((error) => {
       console.log("🚨 初期化に失敗しました", error);
+      return false;
     });
-
-  return false;
 };
