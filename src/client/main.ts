d1fab7a67b86238062b7979b7e5a9e75a207965e
kazuhe <omc.ohara@gmail.com>
2022-09-15 23:47:33 +0900

    Add vue-router

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,5 +1,21 @@
 import { createApp } from "vue";
 import "./style.css";
+import { createRouter, createWebHistory, RouterOptions } from "vue-router";
 import App from "./App.vue";
+import Home from "./pages/index.vue";
+import Memo from "./pages/memo.vue";
 
-createApp(App).mount("#app");
+const routes: RouterOptions["routes"] = [
+  { path: "/", name: "Home", component: Home },
+  { path: "/memo", name: "Memo", component: Memo },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+const app = createApp(App);
+
+app.use(router);
+app.mount("#app");
---
+++ src/client/pages/memo.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h2>Memo</h2>
+    <router-link to="/">to Home</router-link>
+  </div>
+</template>
