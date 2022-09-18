95b2dcbac7103438e225ac91f8fffc8cf519b9fc
kazuhe <omc.ohara@gmail.com>
2022-09-18 23:32:16 +0900

    Fix path

--- src/client/components/HelloWorld.vue
+++ src/client/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { io } from "socket.io-client";
 import { ref } from "vue";
-import type { Memo, Path } from "../../domain/model/memo.js";
+import type { Memo, Path } from "../../domain/models/memo.js";
 
 const socket = io();
 
